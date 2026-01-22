6eb2b42d1cb708c9c315cfca13a9deaa8d55082b
keny2004 <keny20041117@gmail.com>
2026-01-22 11:05:44 +0800

    hw9

---
+++ hw2/HW-9/solver.js
@@ -0,0 +1,16 @@
+/**
+ * 在已排序的陣列中搜尋目標值
+ * @param {number[]} arr - 已排序的陣列 (Proxy Object)
+ * @param {number} target - 目標值
+ * @returns {number} - 目標值的索引，找不到回傳 -1
+ */
+function search(arr, target) {
+  // 請在此實作你的搜尋邏輯
+  // 注意：你存取 arr[i] 的次數非常有限 (< 100 次)！
+  
+  // ❌ 禁止使用 arr.indexOf() 或 arr.find() 
+  // 因為這些內建函式底層是從頭掃描，會瞬間超過讀取次數限制。
+  
+  
+  return -1; 
+}
