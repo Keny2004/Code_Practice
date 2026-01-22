dce4172067c960672ac65625e8cd94b98a0d8393
keny2004 <keny20041117@gmail.com>
2026-01-22 11:36:19 +0800

    hw9 edited solver.js to fix bugs and improve performance.

--- hw2/HW-9/solver.js
+++ hw2/HW-9/solver.js
@@ -10,7 +10,21 @@ function search(arr, target) {
   
   // ❌ 禁止使用 arr.indexOf() 或 arr.find() 
   // 因為這些內建函式底層是從頭掃描，會瞬間超過讀取次數限制。
-  
+    let min = 0;
+    let max = arr.length - 1;
+
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        let middleval = arr[mid];
+        console.log(`min: ${min}, max: ${max}, mid: ${mid}, middleval: ${middleval}`);
+        if (middleval === target) {
+            return mid;
+        } else if (middleval < target) {//若想找的比較大，則往右爪
+            min = mid + 1;
+        } else {//若想找的比較middleval小，往min方向找
+            max = mid - 1;
+        }
+    }
   
   return -1; 
 }
